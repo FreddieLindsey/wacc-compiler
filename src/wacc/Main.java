12d79525ae1881340e26c2e176edee45a1279e11
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-03 14:03:36 +0000

    Basic java program

--- src/wacc/Main.java
+++ src/wacc/Main.java
@@ -1,14 +1,30 @@
 package wacc;
 
+import antlr.BasicLexer;
+import antlr.BasicParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("TODO: MAIN");
+        ANTLRInputStream in = new ANTLRInputStream(System.in);
+
+        BasicLexer lexer = new BasicLexer(in);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        BasicParser parser = new BasicParser(tokens);
+
+        ParseTree tree = parser.program();
+
+        System.out.println(tree.toStringTree(parser));
     }
 
     public static int test_test() {
-        return 0;
+      return 0;
     }
 }
