c653565d279bc7be811420c94245eaf4e9fd1b43
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-11 11:50:45 +0000

    Added ProgramNodeTest to makefile
    Implemented tests for ProgramNode

---
+++ src/wacc/ast/FuncNode.java
@@ -0,0 +1,8 @@
+package wacc.ast;
+
+public class FuncNode implements ASTNode {
+    @Override
+    public boolean isSemanticallyValid() {
+        return false;
+    }
+}
---
+++ src/wacc/ast/ProgramNode.java
@@ -0,0 +1,34 @@
+package wacc.ast;
+
+import java.util.ArrayList;
+
+public class ProgramNode implements ASTNode {
+
+    private ArrayList<FuncNode> funcs;
+    private StatNode stat;
+
+    public ProgramNode() {
+        this.funcs = new ArrayList<>();
+    }
+
+    @Override
+    public boolean isSemanticallyValid() {
+        return true;
+    }
+
+    public void add(FuncNode f) {
+        funcs.add(f);
+    }
+
+    public ArrayList<FuncNode> getFuncs() {
+        return funcs;
+    }
+
+    public StatNode getStat() {
+        return stat;
+    }
+
+    public void setStat(StatNode stat) {
+        this.stat = stat;
+    }
+}
