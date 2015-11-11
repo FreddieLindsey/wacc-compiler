6f968acc652ab8034b0d31f1ce111048c0a86b45
James  Lane <james.lane14@imperial.ac.uk>
2015-11-11 11:35:42 +0000

    Added binary operator enumeration

---
+++ src/wacc/ast/BinaryOperator.java
@@ -0,0 +1,19 @@
+package wacc.ast;
+
+public enum BinaryOperator {
+  
+  MUL, // *
+  DIV, // /
+  MOD, // %
+  ADD, // +
+  SUB, // -
+  GT,  // >
+  GTE, // >=
+  LT,  // <
+  LTE, // <=
+  EQ,  // ==
+  NEQ, // !=
+  AND, // &&
+  OR;  // ||
+
+}
