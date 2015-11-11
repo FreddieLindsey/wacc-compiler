7e06f921e7e2568162de88c380135a5d540b61dc
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-11 09:42:12 +0000

    Added lookup methods

--- src/wacc/symbolTable/SymbolTable.java
+++ src/wacc/symbolTable/SymbolTable.java
@@ -13,4 +13,29 @@ public class SymbolTable {
     dictionary = new HashMap<String, TypeEnum>();
   }
 
+  public void add(String s, TypeEnum t) {
+    dictionary.put(s, t);
+  }
+
+  // Looks up s in the current SymbolTable, returns null if not found
+  // TODO: should this be private?
+  public TypeEnum lookUp(String s) {
+    return dictionary.get(s);
+  }
+
+  // Looks up s in the current and enclosing SymbolTables,
+  // returns null if not found
+  public TypeEnum lookUpAll(String s) {
+    SymbolTable currTable = this;
+    TypeEnum result = null;
+    while (currTable != null) {
+      result = currTable.lookUp(s);
+      if (result != null) {
+        return result;
+      }
+      currTable = currTable.symbolTable;
+    }
+    return null;
+  }
+  
 }
