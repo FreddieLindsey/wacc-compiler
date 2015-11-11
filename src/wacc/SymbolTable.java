cac66a0767ce83f62b599f384067223288c950f6
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-11 09:37:59 +0000

    Added initial files

---
+++ src/wacc/SymbolTable.java
@@ -0,0 +1,10 @@
+package wacc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable {
+
+  private Map<String, TypeEnum> dictionary = new HashMap<String, TypeEnum>();
+
+}
