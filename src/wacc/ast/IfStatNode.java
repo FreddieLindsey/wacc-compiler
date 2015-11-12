5d3c027951f7bf6ffb2c07e32cdfc541201abea7
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 14:01:59 +0000

    Added file

---
+++ src/wacc/ast/IfStatNode.java
@@ -0,0 +1,23 @@
+package wacc.ast;
+
+public class IfStatNode implements StatNode {
+
+	private ExprNode expr;
+	private StatNode stat1;
+	private StatNode stat2;
+
+	public IfStatNode(ExprNode expr, StatNode stat1, StatNode stat2) {
+    this.expr = expr;
+    this.stat1 = stat1;
+    this.stat2 = stat2;
+	}
+
+	@Override
+	public boolean isSemanticallyValid() {
+    //check expr type is bool & semantically valid
+    return expr.isSemanticallyValid()
+        && stat1.isSemanticallyValid() 
+        && stat2.isSemanticallyValid();
+	}
+
+}
