ae33333db2740bc1f5b9e6538733019f948b289f
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 15:10:35 +0000

    Added IdentNode

--- src/wacc/ast/IdentNode.java
+++ src/wacc/ast/IdentNode.java
@@ -1,17 +1,17 @@
 package wacc.ast;
-+
-+public class IdentNode implements ASTNode {
-+  
-+  private String ident;
-+  
-+  public IdentNode(String ident) {
-+    this.ident = ident;
-+  }
-+
-+  @Override
-+  public boolean isSemanticallyValid() {
-+    // TODO Auto-generated method stub
-+    return false;
-+  }
-+
-+}
+
+public class IdentNode implements ASTNode {
+
+  private String ident;
+
+  public IdentNode(String ident) {
+    this.ident = ident;
+  }
+
+  @Override
+  public boolean isSemanticallyValid() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
