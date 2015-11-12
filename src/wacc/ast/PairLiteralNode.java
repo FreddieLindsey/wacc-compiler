e1eb56cc6c4deb9577b46f4532d4d13dbbf7a0ca
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 15:32:41 +0000

    Added file

---
+++ src/wacc/ast/PairLiteralNode.java
@@ -0,0 +1,17 @@
+package wacc.ast;
+
+public class PairLiteralNode extends LiteralNode {
+  
+  // TODO: not sure what pair-liter is supposed to represent
+  
+  public PairLiteralNode() {
+    this.value = null;
+  }
+
+  @Override
+  public boolean isSemanticallyValid() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
