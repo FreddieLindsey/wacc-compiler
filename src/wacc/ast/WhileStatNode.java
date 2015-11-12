b275477d1357367e60385b09f80ed9f3b13021e8
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 14:07:26 +0000

    Added file

---
+++ src/wacc/ast/WhileStatNode.java
@@ -0,0 +1,19 @@
+package wacc.ast;
+
+public class WhileStatNode implements StatNode {
+
+	private ExprNode expr;
+	private StatNode stat;
+
+	public WhileStatNode(ExprNode expr, StatNode stat) {
+  	this.expr = expr;
+  	this.stat = stat;
+	}
+
+	@Override
+	public boolean isSemanticallyValid() {
+    //check expr type is bool & semantically valid
+    return stat.isSemanticallyValid();
+	}
+
+}
