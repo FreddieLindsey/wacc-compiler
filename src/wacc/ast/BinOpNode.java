e3355a5bca909023a09a6c05bcf61b87b3f68fa5
James Prince <Prince@dyn1238-72.wlan.ic.ac.uk>
2015-11-12 16:49:24 +0000

    Added so much love

--- src/wacc/ast/BinOpNode.java
+++ src/wacc/ast/BinOpNode.java
@@ -18,8 +18,31 @@ public class BinOpNode implements ExprNode {
   public boolean isSemanticallyValid() {
     //TODO: check lhs and rhs collpase down to same type
     //TODO: check that these matching types can have the operator applied to them
-    return lhs.isSemanticallyValid()
-        && rhs.isSemanticallyValid();
+    boolean valid = true;
+
+    valid &= lhs.isSemanticallyValid()
+          && rhs.isSemanticallyValid();
+
+    valid &= lhs.type() = rhs.type();
+
+    switch(op) {
+      case MUL:
+      case DIV:
+      case MOD:
+      case ADD:
+      case SUB: valid &= lhs.type() == TypeEnum.INT; break;
+      case GT :
+      case GTE:
+      case LT :
+      case LTE: valid &= (lhs.type() == TypeEnum.INT
+                        ||lhs.type() == TypeEnum.CHAR; break; 
+      case EQ :
+      case NEQ: valid &= (lhs.type() != TypeEnum.STRING; break;
+      case AND:
+      case OR : valid &= (lhs.type() == TypeEnum.BOOL;
+    }
+
+    return semValid;
   }
 
 }
