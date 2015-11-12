82ea8274c36caa061d0ea56221f3c2a8c128eb31
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 14:08:59 +0000

    Added file

---
+++ src/wacc/ast/CompStatNode.java
@@ -0,0 +1,22 @@
+package wacc.ast;
+
+public class CompStatNode implements StatNode {
+
+	// for stat composition
+
+
+	private StatNode stat1;
+	private StatNode stat2;
+
+	public CompStatNode(StatNode stat1, StatNode stat2) {
+		this.stat1 = stat1;
+		this.stat2 = stat2;
+	}
+
+	@Override
+	public boolean isSemanticallyValid() {
+		return stat1.isSemanticallyValid() && stat2.isSemanticallyValid();
+	}
+
+
+}
