18a6cd17347ae70f032a7f5067ec6d8846894660
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 13:59:43 +0000

    Added file

---
+++ src/wacc/ast/BasicStatNode.java
@@ -0,0 +1,27 @@
+package wacc.ast;
+
+public class BasicStatNode implements StatNode {
+  
+  private ExprNode expr;
+  private StatTypeEnum st;
+
+  public BasicStatNode(StatTypeEnum st, ExprNode expr) {
+    this.st = st;
+    this.expr = expr;
+  }
+
+  @Override
+  public boolean isSemanticallyValid() {
+    switch (st) {
+	    case SKIP:
+	  	case READ:
+	  	case FREE:
+	  	case RETURN:
+	  	case EXIT:
+	  	case PRINT:
+	  	case PRINTLN: return expr.isSemanticallyValid();
+	  	default: return false;
+    }
+  }
+
+}
