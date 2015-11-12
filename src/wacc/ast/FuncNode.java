e008b4190f3dcc4359196cd617308dc194556cde
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 14:57:04 +0000

    Alignment

--- src/wacc/ast/FuncNode.java
+++ src/wacc/ast/FuncNode.java
@@ -2,20 +2,20 @@ package wacc.ast;
 
 public class FuncNode implements ASTNode {
 
-	private TypeNode t;
-	private IdentNode n;
-	private ParamListNode ps;
-	private StatNode stat;
+  private TypeNode t;
+  private IdentNode n;
+  private ParamListNode ps;
+  private StatNode stat;
 
-	public FuncNode(TypeNode t, IdentNode n, ParamListNode ps, StatNode stat) {
+  public FuncNode(TypeNode t, IdentNode n, ParamListNode ps, StatNode stat) {
     this.t = t;
     this.n = n;
     this.ps = ps;
     this.stat = stat;
-	}
+  }
 
-	@Override
-	public boolean isSemanticallyValid() {
+  @Override
+  public boolean isSemanticallyValid() {
     return t.isSemanticallyValid()
         && n.isSemanticallyValid()
         && ps.isSemanticallyValid()
