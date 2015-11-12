fdd8694adb9316f52b6af4051ecaaf738c24fc8f
James Prince <Prince@dyn1238-72.wlan.ic.ac.uk>
2015-11-12 15:54:34 +0000

    removed comment

--- src/wacc/ast/ReAssignNode.java
+++ src/wacc/ast/ReAssignNode.java
@@ -14,7 +14,6 @@ public class ReAssignNode implements StatNode {
 	@Override
 	public boolean isSemanticallyValid() {
     //TODO: check that the lhs isnt already being used in same scope
-	// TODO: check types match
     return lhs.isSemanticallyValid() 
     	&& rhs.isSemanticallyValid()
     	&& lhs.type() == rhs.type();
