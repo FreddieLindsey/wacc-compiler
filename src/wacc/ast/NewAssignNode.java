6adad80a38100f0c20f9aed947989ec7c0d59cd8
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 14:03:11 +0000

    Added file

---
+++ src/wacc/ast/NewAssignNode.java
@@ -0,0 +1,23 @@
+package wacc.ast;
+
+public class NewAssignNode implements StatNode {
+
+	private TypeNode t;
+	private IdentNode i;
+  private AssignRHSNode rhs;
+
+	public NewAssignNode(TypeNode t, IdentNode i, AssignRHSNode rhs) {
+    this.t = t;
+    this.i = i;
+    this.rhs = rhs;
+  }
+
+
+	@Override
+	public boolean isSemanticallyValid() {
+    //TODO: check that the Ident isnt already being used in same scope
+    //TODO: check type is valid?
+    return rhs.isSemanticallyValid();
+	}
+
+}
