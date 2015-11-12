44766a309b80ee207d29ecf6ee2a6804a72d47e1
James Lane <james.lane14@imperial.ac.uk>
2015-11-12 15:39:26 +0000

    Implemented validity check for IdentNode

--- src/wacc/ast/IdentNode.java
+++ src/wacc/ast/IdentNode.java
@@ -10,8 +10,26 @@ public class IdentNode implements ASTNode {
 
   @Override
   public boolean isSemanticallyValid() {
-    // TODO Auto-generated method stub
-    return false;
+    if (ident.length() < 1) {
+      return false;
+    }
+    if (!isFirstValid(ident.charAt(0))) {
+      return false;
+    }
+    for (int i = 1; i < ident.length(); ++i) {
+      if (!areRestValid(ident.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean isFirstValid(char c) {
+    return c == '_' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+  }
+
+  private boolean areRestValid(char c) {
+    return (c >= '0' && c <= '9') || isFirstValid(c);
   }
 
 }
