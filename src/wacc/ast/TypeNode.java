f35ce24fd6c25745fd6dcd05d8f3cba3c2bd72e8
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-12 14:07:06 +0000

    Added file

---
+++ src/wacc/ast/TypeNode.java
@@ -0,0 +1,12 @@
+package wacc.ast;
+
+public class TypeNode implements StatNode {
+
+
+	@Override
+	public boolean isSemanticallyValid() {
+    //TODO: check type exists?
+    return false;
+	}
+
+}
