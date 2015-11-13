afb66de2b479efcd985994dcdf9b8cc6ff8ea2b5
James Long <me@james-long.com>
2015-11-13 18:28:06 +0000

    Checked out test file

---
+++ test/wacc/ast/NewAssignNodeTest.java
@@ -0,0 +1,34 @@
+package wacc.ast;
+
+import org.junit.Test;
+
+import wacc.symbolTable.TypeEnum;
+
+public class NewAssignNodeTest {
+
+  private NewAssignNode n;
+
+  private TypeEnum t = TypeEnum.STRING;
+  private IdentNode i = new IdentNode("test");
+  private AssignRHSNode rhs = new IdentNode("test2");
+
+  @Test
+  public void newAssignNodeInit() {
+    n = new NewAssignNode(t, i, rhs);
+    assert(n.getType() == t);
+    assert(n.getIdent().equals(i));
+    assert(n.getRHS().equals(rhs));
+  }
+
+  @Test
+  public void newAssignNodeValid() {
+    n = new NewAssignNode(t, i, rhs);
+    assert(n.isSemanticallyValid() ==
+          (
+            i.isSemanticallyValid()
+            && rhs.isSemanticallyValid()
+            && t == rhs.type()
+          ));
+  }
+
+}
