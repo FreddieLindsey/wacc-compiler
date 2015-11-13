5e8a039022b2eba8ff753b52e3d105e40d053296
James Prince <Prince@Jamess-MBP-2.home>
2015-11-13 18:01:02 +0000

    added not valid test

--- test/wacc/ast/CompStatNodeTest.java
+++ test/wacc/ast/CompStatNodeTest.java
@@ -1,11 +1,13 @@
 package wacc.ast;
 
 import org.junit.Test;
+import wacc.ast.StatTypeEnum;
 
 public class CompStatNodeTest {
 
-	StatNode s1 = new StatNode(TypeNodeEnum.SKIP, null);
-  StatNode s2 = new StatNode(TypeNodeEnum.SKIP, null);
+	StatNode s1 = new BasicStatNode(StatTypeEnum.SKIP, null);
+  StatNode s2 = new BasicStatNode(StatTypeEnum.SKIP, null);
+  StatNode s3 = new BasicStatNode(StatTypeEnum.RETURN, null); //invalid
   CompStatNode c;
 
 
@@ -22,10 +24,10 @@ public class CompStatNodeTest {
       assert(c.isSemanticallyValid());
   }
 
-  // @Test
-  // public void compStatNodeNotValid() {
-  //     c = new CompStatNode(s1, s2);
-  //     assert(!c.isSemanticallyValid());
-  // }
+  @Test
+  public void compStatNodeNotValid() {
+      c = new CompStatNode(s1, s3);
+      assert(!c.isSemanticallyValid());
+  }
 
 }
