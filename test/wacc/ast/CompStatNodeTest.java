2cb0f912570e3876a2a56d33c57ab07d1d0f44d4
James Prince <Prince@Jamess-MBP-2.home>
2015-11-13 17:54:15 +0000

    meh

--- test/wacc/ast/CompStatNodeTest.java
+++ test/wacc/ast/CompStatNodeTest.java
@@ -4,9 +4,28 @@ import org.junit.Test;
 
 public class CompStatNodeTest {
 
+	StatNode s1 = new StatNode(TypeNodeEnum.SKIP, null);
+  StatNode s2 = new StatNode(TypeNodeEnum.SKIP, null);
+  CompStatNode c;
+
+
   @Test
-  public void removethisFunction() {
-    assert(true);
+  public void compStatNodeInit() {
+    c = new CompStatNode(s1, s2);
+    assert(c.getFstStat().equals(s1));
+    assert(c.getSndStat().equals(s2));
   }
+
+  @Test
+  public void compStatNodeValidity() {
+      c = new CompStatNode(s1, s2);
+      assert(c.isSemanticallyValid());
+  }
+
+  // @Test
+  // public void compStatNodeNotValid() {
+  //     c = new CompStatNode(s1, s2);
+  //     assert(!c.isSemanticallyValid());
+  // }
 
 }
