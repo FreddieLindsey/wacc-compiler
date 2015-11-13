5d4c8b69eb4a5fb3e8cbbef36f3d386b64a7a345
James Long <me@james-long.com>
2015-11-13 18:32:15 +0000

    Tests

--- src/wacc/ast/ReAssignNode.java
+++ src/wacc/ast/ReAssignNode.java
@@ -2,21 +2,28 @@ package wacc.ast;
 
 public class ReAssignNode implements StatNode {
 
-	private AssignLHSNode lhs;
-	private AssignRHSNode rhs;
+    private AssignLHSNode lhs;
+    private AssignRHSNode rhs;
 
-	public ReAssignNode(AssignLHSNode lhs, AssignRHSNode rhs) {
-    	this.lhs = lhs;
-    	this.rhs = rhs;
-  	}
+    public ReAssignNode(AssignLHSNode lhs, AssignRHSNode rhs) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
 
+    public AssignLHSNode getLHS() {
+        return lhs;
+    }
 
-	@Override
-	public boolean isSemanticallyValid() {
+    public AssignRHSNode getRHS() {
+        return rhs;
+    }
+
+    @Override
+    public boolean isSemanticallyValid() {
     //TODO: check that the lhs isnt already being used in same scope
     return lhs.isSemanticallyValid() 
-    	&& rhs.isSemanticallyValid()
-    	&& lhs.type() == rhs.type();
-	}
+        && rhs.isSemanticallyValid()
+        && lhs.type() == rhs.type();
+    }
 
 }
