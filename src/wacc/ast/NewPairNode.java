92b4a4bcfe263a4790b51dfd9d7ed1fc7c902cc1
James Prince <Prince@Jamess-MacBook-Pro-2.local>
2015-11-14 13:16:09 +0000

    added file and (uncommented) tests (has dependencies)

---
+++ src/wacc/ast/NewPairNode.java
@@ -0,0 +1,40 @@
+package wacc.ast;
+
+import wacc.symbolTable.TypeEnum;
+
+public class NewPairNode implements AssignRHSNode {
+
+  private ExprNode fst;
+  private ExprNode snd;
+
+  public NewPairNode(ExprNode fst, ExprNode snd) {
+    this.fst = fst;
+    this.snd = snd;
+  }
+
+  public ExprNode getFst() {
+    return this.fst;
+  }
+
+  public ExprNode getSnd() {
+    return this.snd;
+  }
+
+  @Override
+  public TypeEnum type() {
+    //symbol table?
+    return TypeEnum.PAIR;
+  }
+
+  @Override
+  public boolean isSemanticallyValid() {
+    //don't need to know anything about types!
+    return fst.isSemanticallyValid()
+        && snd.isSemanticallyValid();
+  }
+
+
+
+
+
+}
---
+++ test/wacc/ast/NewPairNodeTest.java
@@ -0,0 +1,42 @@
+package wacc.ast;
+
+import org.junit.Test;
+
+public class NewPairNodeTest {
+
+    @Test
+    public void removethis() {
+        assert(true);
+    }
+
+    // private NewPairNode n;
+    // private ExprNode e1 = new IntNode(5);
+    // private ExprNode e2 = new IntNode(10);
+    // private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000);
+
+    // @Test
+    // public void pairElemInit() {
+    //     n = new NewPairNode(e1, e2);
+    //     assert(n.getFst().equals(e1));
+    //     assert(n.getSnd().equals(e2));
+    // }
+
+    // @Test
+    // public void pairElemValidityT() {
+    //     n = new NewPairNode(e1, e2);
+
+    //     assert(e1.isSemanticallyValid());
+    //     assert(e2.isSemanticallyValid());
+    //     assert(n.isSemanticallyValid());
+    // }
+
+    // @Test
+    // public void pairElemValidityF() {
+    //     n = new NewPairNode(e1, e3);
+
+    //     assert(e1.isSemanticallyValid());
+    //     assert(!e3.isSemanticallyValid());
+    //     assert(!n.isSemanticallyValid());
+    // }
+
+}
