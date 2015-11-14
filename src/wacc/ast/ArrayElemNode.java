463ae309dbbe355d4a1252badeebb92b1fa7fa2d
James Prince <Prince@Jamess-MacBook-Pro-2.local>
2015-11-14 12:32:15 +0000

    added getters and tpye method as required by AssignLHSNode

--- src/wacc/ast/ArrayElemNode.java
+++ src/wacc/ast/ArrayElemNode.java
@@ -1,10 +1,10 @@
 package wacc.ast;
 
-public class ArrayElemNode implements ASTNode {
+import wacc.symbolTable.TypeEnum;
+
+public class ArrayElemNode implements ASTNode, AssignLHSNode {
 
   //for array access eg. a[0]
-  //TODO: handle out of bounds access problems
-
 
   private IdentNode ident;
   private ExprNode expr;
@@ -14,9 +14,25 @@ public class ArrayElemNode implements ASTNode {
     this.expr  = expr;
   }
 
+  public TypeEnum type() {
+    //should return primitive type using symbol table to figure out what kind of array it is
+    return TypeEnum.ARR;
+  }
+
+  public IdentNode getId() {
+    return this.ident;
+  }
+
+  public ExprNode getExpr() {
+    return this.expr;
+  }
+
   @Override
   public boolean isSemanticallyValid() {
-    return ident.isSemanticallyValid() && expr.isSemanticallyValid();
+    //TODO: handle out of bounds access problems, should this be in the symbol table?
+    return ident.isSemanticallyValid() 
+        && expr.isSemanticallyValid()
+        && expr.type() == TypeEnum.INT;
   }
 
 }
---
+++ test/wacc/ast/ArrayElemNodeTest.java
@@ -0,0 +1,58 @@
+package wacc.ast;
+
+import org.junit.Test;
+
+public class ArrayElemNodeTest {
+
+    @Test
+    public void removethis() {
+        assert(true);
+    }
+
+    // private ArrayElem a;
+    // private IdentNode i1 = new IdentNode("x");
+    // private IdentNode i2 = new IdentNode("1hgklsjkf"); //invalid
+    // private ExprNode e1 = new BoolNode(true); // invalid
+    // private ExprNode e2 = new IntNode(5);
+
+    // @Test
+    // public void arrayElemNodeInit() {
+    //     a = new ArrayElem(i1, e2);
+    //     assert(a.getId().equals(i1));
+    //     assert(a.getExpr().equals(e2));
+    // }
+
+    // @Test
+    // public void arrayElemValidityT() {
+    //     a = new ArrayElem(i1, e2);
+    //     assert(i1.isSemanticallyValid());
+    //     assert(e2.isSemanticallyValid());
+    //     assert(a.isSemanticallyValid());
+    // }
+
+    // @Test
+    // public void arrayElemValidityF1() {
+    //     a = new ArrayElem(i1, e1);
+    //     assert(i1.isSemanticallyValid());
+    //     assert(!e1.isSemanticallyValid());
+    //     assert(!a.isSemanticallyValid());
+    // }
+
+    // @Test
+    // public void arrayElemValidityF2() {
+    //     a = new ArrayElem(i2, e2);
+    //     assert(!i2.isSemanticallyValid());
+    //     assert(e2.isSemanticallyValid());
+    //     assert(!a.isSemanticallyValid());
+    // }
+
+
+    // @Test
+    // public void arrayElemValidityF3() {
+    //     a = new ArrayElem(i2, e1);
+    //     assert(!i2.isSemanticallyValid());
+    //     assert(!e1.isSemanticallyValid());
+    //     assert(!a.isSemanticallyValid());
+    // }
+
+}
