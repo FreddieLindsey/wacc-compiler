590cbd4d99e8c4aaf1e95759751a0cf7a39c190e
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-14 16:15:44 +0000

    Added tests

--- test/wacc/ast/ArgListNodeTest.java
+++ test/wacc/ast/ArgListNodeTest.java
@@ -2,42 +2,39 @@ package wacc.ast;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class ArgListNodeTest {
 
-    @Test
-    public void removethis() {
-        assert(true);
-    }
+  private ArgListNode a;
+  private ExprNode e1 = new IntNode(5);
+  private ExprNode e2 = new IntNode(10);
+  private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000);
 
-    // private ArgListNode a;
-    // private ExprNode e1 = new IntNode(5);
-    // private ExprNode e2 = new IntNode(10);
-    // private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000);
+  @Test
+  public void pairElemInit() {
+    a = new ArgListNode(new ExprNode[]{e1, e2});
 
-    // @Test
-    // public void pairElemInit() {
-    //     a = new ArgListNode(new ExprNode[]{e1, e2});
+    assertTrue(a.getExprs()[0].equals(e1));
+    assertTrue(a.getExprs()[1].equals(e2));
+  }
 
-    //     assert(a.getExprs()[0].equals(e1));
-    //     assert(a.getExprs()[1].equals(e2));
-    // }
+  @Test
+  public void pairElemValidityT() {
+    a = new ArgListNode(new ExprNode[]{e1, e2});
 
-    // @Test
-    // public void pairElemValidityT() {
-    //     a = new ArgListNode(new ExprNode[]{e1, e2});
+    assertTrue(e1.isSemanticallyValid());
+    assertTrue(e2.isSemanticallyValid());
+    assertTrue(a.isSemanticallyValid());
+  }
 
-    //     assert(e1.isSemanticallyValid());
-    //     assert(e2.isSemanticallyValid());
-    //     assert(a.isSemanticallyValid());
-    // }
+  @Test
+  public void pairElemValidityF() {
+    a = new ArgListNode(new ExprNode[]{e1, e3});
 
-    // @Test
-    // public void pairElemValidityF() {
-    //     a = new ArgListNode(new ExprNode[]{e1, e3});
-
-    //     assert(e1.isSemanticallyValid());
-    //     assert(!e3.isSemanticallyValid());
-    //     assert(!a.isSemanticallyValid());
-    // }
+    assertTrue(e1.isSemanticallyValid());
+    assertTrue(!e3.isSemanticallyValid());
+    assertTrue(!a.isSemanticallyValid());
+  }
 
 }
