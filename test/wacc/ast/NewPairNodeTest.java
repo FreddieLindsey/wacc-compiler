1dd7d8836fa00e53022e3a99eb70157d42381af1
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-14 16:29:48 +0000

    Added tests

--- test/wacc/ast/NewPairNodeTest.java
+++ test/wacc/ast/NewPairNodeTest.java
@@ -2,41 +2,36 @@ package wacc.ast;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class NewPairNodeTest {
 
-    @Test
-    public void removethis() {
-        assert(true);
-    }
+  private NewPairNode n;
+  private ExprNode e1 = new IntNode(5);
+  private ExprNode e2 = new IntNode(10);
+  private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000L);
 
-    // private NewPairNode n;
-    // private ExprNode e1 = new IntNode(5);
-    // private ExprNode e2 = new IntNode(10);
-    // private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000);
+  @Test
+  public void pairElemInit() {
+    n = new NewPairNode(e1, e2);
+    assertTrue(n.getFst().equals(e1));
+    assertTrue(n.getSnd().equals(e2));
+  }
 
-    // @Test
-    // public void pairElemInit() {
-    //     n = new NewPairNode(e1, e2);
-    //     assert(n.getFst().equals(e1));
-    //     assert(n.getSnd().equals(e2));
-    // }
+  @Test
+  public void pairElemValidityT() {
+    n = new NewPairNode(e1, e2);
+    assertTrue(e1.isSemanticallyValid());
+    assertTrue(e2.isSemanticallyValid());
+    assertTrue(n.isSemanticallyValid());
+  }
 
-    // @Test
-    // public void pairElemValidityT() {
-    //     n = new NewPairNode(e1, e2);
-
-    //     assert(e1.isSemanticallyValid());
-    //     assert(e2.isSemanticallyValid());
-    //     assert(n.isSemanticallyValid());
-    // }
-
-    // @Test
-    // public void pairElemValidityF() {
-    //     n = new NewPairNode(e1, e3);
-
-    //     assert(e1.isSemanticallyValid());
-    //     assert(!e3.isSemanticallyValid());
-    //     assert(!n.isSemanticallyValid());
-    // }
+  @Test
+  public void pairElemValidityF() {
+    n = new NewPairNode(e1, e3);
+    assertTrue(e1.isSemanticallyValid());
+    assertTrue(!e3.isSemanticallyValid());
+    assertTrue(!n.isSemanticallyValid());
+  }
 
 }
