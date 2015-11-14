0102e7ab4ae33a722859eac7e9b2c2aefea223e8
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-14 09:15:26 +0000

    Removed comments and updated implementation of PairLiteralNode

--- src/wacc/ast/PairLiteralNode.java
+++ src/wacc/ast/PairLiteralNode.java
@@ -2,16 +2,13 @@ package wacc.ast;
 
 public class PairLiteralNode extends LiteralNode {
   
-  // TODO: not sure what pair-liter is supposed to represent
-  
   public PairLiteralNode() {
     this.value = null;
   }
 
   @Override
   public boolean isSemanticallyValid() {
-    // TODO Auto-generated method stub
-    return false;
+    return true;
   }
 
 }
