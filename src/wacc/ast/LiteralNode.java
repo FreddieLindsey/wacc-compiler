e96781fadc4392d43d5617461c5a188e2af9e28f
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-14 13:58:39 +0000

    Removed ASTNode from LiteralNode
    ExprNode already extends ASTNode

--- src/wacc/ast/LiteralNode.java
+++ src/wacc/ast/LiteralNode.java
@@ -1,9 +1,9 @@
 package wacc.ast;
 
-public abstract class LiteralNode<T> implements ASTNode, ExprNode {
-  
+public abstract class LiteralNode<T> implements ExprNode {
+
   protected T value;
-  
+
   // Returns the value held by the literal node
   public T getValue() {
     return value;
