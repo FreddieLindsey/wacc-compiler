2074023332c5e7fa0b30a06d2ae7500fd824c466
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-14 16:12:10 +0000

    Added callNodeTest back in

--- test/wacc/ast/CallNodeTest.java
+++ test/wacc/ast/CallNodeTest.java
@@ -2,46 +2,43 @@ package wacc.ast;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class CallNodeTest {
 
-    @Test
-    public void removethis() {
-        assert(true);
-    }
-
-    // private CallNode c;
-    // private IdentNode ident = new IdentNode("x");
-    // private ExprNode e1 = new IntNode(5);
-    // private ExprNode e2 = new IntNode(10);
-    // private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000); //invalid
-    // private ArgListNode argsV = new ArgListNode(new ExprNode[]{e1, e2});
-    // private ArgListNode argsINV = new ArgListNode(new ExprNode[]{e1, e2, e3}); //invalid
+  private CallNode c;
+  private IdentNode ident = new IdentNode("x");
+  private ExprNode e1 = new IntNode(5);
+  private ExprNode e2 = new IntNode(10);
+  private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000L); //invalid
+  private ArgListNode argsV = new ArgListNode(new ExprNode[]{e1, e2});
+  private ArgListNode argsINV = new ArgListNode(new ExprNode[]{e1, e2, e3}); //invalid
 
 
-    // @Test
-    // public void callInit() {
-    //     c = new CallNode(ident, argsV);
+  @Test
+  public void callInit() {
+    c = new CallNode(ident, argsV);
 
-    //     assert(c.getIdent().equals(ident));
-    //     assert(c.getArgs().equals(argsV));
-    // }
+    assertTrue(c.getIdent().equals(ident));
+    assertTrue(c.getArgs().equals(argsV));
+  }
 
-    // @Test
-    // public void callValidityT() {
-    //     c = new CallNode(ident, argsV);
+  @Test
+  public void callValidityT() {
+    c = new CallNode(ident, argsV);
 
-    //     assert(ident.isSemanticallyValid());
-    //     assert(argsV.isSemanticallyValid());
-    //     assert(c.isSemanticallyValid());
-    // }
+    assertTrue(ident.isSemanticallyValid());
+    assertTrue(argsV.isSemanticallyValid());
+    assertTrue(c.isSemanticallyValid());
+  }
 
-    // @Test
-    // public void callValidityF() {
-    //     c = new CallNode(ident, argsV);
+  @Test
+  public void callValidityF() {
+    c = new CallNode(ident, argsV);
 
-    //     assert(ident.isSemanticallyValid());
-    //     assert(!argsINV.isSemanticallyValid());
-    //     assert(!c.isSemanticallyValid());
-    // }
+    assertTrue(ident.isSemanticallyValid());
+    assertTrue(!argsINV.isSemanticallyValid());
+    assertTrue(!c.isSemanticallyValid());
+  }
 
 }
