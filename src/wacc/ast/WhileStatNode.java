80fa6309131d8d8b7731f36fcb46b6c3831fc574
James Prince <Prince@Jamess-MacBook-Pro-2.local>
2015-11-14 09:04:04 +0000

    Added bool check

--- src/wacc/ast/WhileStatNode.java
+++ src/wacc/ast/WhileStatNode.java
@@ -2,18 +2,20 @@ package wacc.ast;
 
 public class WhileStatNode implements StatNode {
 
-	private ExprNode expr;
-	private StatNode stat;
+  private ExprNode expr;
+  private StatNode stat;
 
-	public WhileStatNode(ExprNode expr, StatNode stat) {
-  	this.expr = expr;
-  	this.stat = stat;
-	}
+  public WhileStatNode(ExprNode expr, StatNode stat) {
+    this.expr = expr;
+    this.stat = stat;
+  }
 
-	@Override
-	public boolean isSemanticallyValid() {
+  @Override
+  public boolean isSemanticallyValid() {
     //check expr type is bool & semantically valid
-    return stat.isSemanticallyValid();
-	}
+    return stat.isSemanticallyValid()
+        && expr.isSemanticallyValid()
+        && expr.type() == TypeEnum.BOOL;
+  }
 
 }
