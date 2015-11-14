f538a9b895047a8ff8f4eb7cab775cecc0bd2d01
James Prince <Prince@Jamess-MBP-2.home>
2015-11-14 14:34:45 +0000

    changed paramnode type to array

--- src/wacc/ast/ParamListNode.java
+++ src/wacc/ast/ParamListNode.java
@@ -2,16 +2,21 @@ package wacc.ast;
 
 public class ParamListNode implements ASTNode {
 
-	private ParamNode ps;
+  private ParamNode[] ps;
 
-	public ParamListNode(ParamNode ps) {
-   	this.ps = ps;
-	}
-
-	@Override
-	public boolean isSemanticallyValid() {
-    return ps.isSemanticallyValid();
+  public ParamListNode(ParamNode[] ps) {
+     this.ps = ps;
   }
 
+  @Override
+  public boolean isSemanticallyValid() {
 
+    for (int i = 0; i < ps.length ; i++) {
+      if (!ps[i].isSemanticallyValid()) {
+          return false;
+      }
+    }
+
+    return true;
+  }
 }
