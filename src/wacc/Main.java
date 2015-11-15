2de0423bee7f3b347b9becf4f5635972cd17920a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-15 17:40:21 +0000

    Added bulk of IO for testing valid tests

--- src/wacc/Main.java
+++ src/wacc/Main.java
@@ -5,26 +5,44 @@ import antlr.BasicParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import wacc.ast.ASTTree;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.FileVisitResult;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        ANTLRInputStream in = new ANTLRInputStream(System.in);
+  public static void main(String[] args) throws IOException {
+    ANTLRInputStream in = new ANTLRInputStream(System.in);
 
-        BasicLexer lexer = new BasicLexer(in);
+    BasicLexer lexer = new BasicLexer(in);
 
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        BasicParser parser = new BasicParser(tokens);
+    BasicParser parser = new BasicParser(tokens);
 
-        ParseTree tree = parser.program();
+    ParseTree tree = parser.program();
 
-        System.out.println(tree.toStringTree(parser));
-    }
+    System.out.println(tree.toStringTree(parser));
+  }
 
-    public static boolean test_test() {
-      return true;
-    }
+  public static boolean test_test() {
+    return true;
+  }
+
+  public static ParseTree parseInput(InputStream i) throws IOException {
+    ANTLRInputStream in = new ANTLRInputStream(i);
+
+    BasicLexer lexer = new BasicLexer(in);
+
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    BasicParser parser = new BasicParser(tokens);
+
+    return parser.program();
+  }
+
+  public static ASTTree analyseFile(ParseTree parseTree) throws IOException {
+    return new ASTTree(parseTree);
+  }
 }
--- test/wacc/MainTest.java
+++ test/wacc/MainTest.java
@@ -1,13 +1,69 @@
 package wacc;
 
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
+import wacc.ast.ASTTree;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Iterator;
+
+import static java.nio.file.Files.walkFileTree;
 import static org.junit.Assert.*;
 
 public class MainTest {
 
-    @Test
-    public void test_test() {
-        assertTrue(Main.test_test());
-    }
+  @Test
+  public void test_test() {
+    assertTrue(Main.test_test());
+  }
+
+  @Test
+  public void validTests() throws IOException {
+    walkFileTree(FileSystems.getDefault().getPath("wacc", "valid"), new FileVisitor<Path>() {
+      @Override
+      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return null;
+      }
+
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (file.toString().endsWith(".wacc")) {
+          try {
+            ParseTree pt = Main.parseInput(new FileInputStream(String.valueOf(file)));
+            ASTTree ast = Main.analyseFile(pt);
+          } catch (IOException e) {
+            System.out.println("Encountered an error parsing/analysing file: "
+              + file.toString() + e);
+          }
+        }
+
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return null;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return null;
+      }
+    });
+  }
+
+  @Test
+  public void invalidSyntaxTests() {
+
+  }
+
+  @Test
+  public void invalidSemanticTests() {
+
+  }
 
 }
