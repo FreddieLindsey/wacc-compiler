2f4f2f052937963456fedfb03c9570b7dcdd6a27
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-15 11:47:31 +0000

    Uncommented tests

--- test/wacc/ast/WhileStatNodeTest.java
+++ test/wacc/ast/WhileStatNodeTest.java
@@ -3,45 +3,42 @@ package wacc.ast;
 import org.junit.Test;
 import wacc.symbolTable.TypeEnum;
 
+import static org.junit.Assert.*;
+
 public class WhileStatNodeTest {
 
   private WhileStatNode w;
 
-  @Test
-  public void removeThis() {
-    assert(true);
-  }
+   private ExprNode e = new BoolNode(true);
+   private StatNode s1 = new BasicStatNode(StatTypeEnum.SKIP, null);
+   private StatNode s2 = new BasicStatNode(StatTypeEnum.RETURN, null); //invalid
 
-  // private ExprNode e = BoolNode(true);
-  // private StatNode s1 = new BasicStatNode(StatTypeEnum.SKIP, null);
-  // private StatNode s2 = new BasicStatNode(StatTypeEnum.RETURN, null); //invalid
+   @Test
+   public void whileStatNodeInit() {
+     w = new WhileStatNode(e, s1);
+     assertTrue(w.getExpr().equals(e));
+     assertTrue(w.getStat().equals(s1));
+   }
 
-  // @Test
-  // public void whileStatNodeInit() {
-  //   w = new WhileStatNode(e, s1);
-  //   assert(w.getExpr().equals(e));
-  //   assert(w.getStat().equals(s1));
-  // }
+   @Test
+   public void whileStatNodeValidT() {
+     w = new WhileStatNode(e, s1);
+     assertTrue(e.isSemanticallyValid());
+     assertTrue(s1.isSemanticallyValid());
+     assertTrue(w.isSemanticallyValid());
+   }
 
-  // @Test
-  // public void whileStatNodeValidT() {
-  //   w = new WhileStatNode(e, s1);
-  //   assert(e.isSemanticallyValid());
-  //   assert(s1.isSemanticallyValid());
-  //   assert(w.isSemanticallyValid());
-  // }
+   @Test
+   public void whileStatNodeValidF() {
+     w = new WhileStatNode(e, s2);
+     assertTrue(e.isSemanticallyValid());
+     assertTrue(!s1.isSemanticallyValid());
+     assertTrue(!w.isSemanticallyValid());
+   }
 
-  // @Test
-  // public void whileStatNodeValidF() {
-  //   w = new WhileStatNode(e, s2);
-  //   assert(e.isSemanticallyValid());
-  //   assert(!s1.isSemanticallyValid());
-  //   assert(!w.isSemanticallyValid());
-  // }
-
-  // @Test
-  // public void whileStatNodeValidF2() {
-  //   //need to check when expr is not a bool
-  // }
+   @Test
+   public void whileStatNodeValidF2() {
+     //need to check when expr is not a bool
+   }
 
 }
