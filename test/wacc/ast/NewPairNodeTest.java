a29af457c214bbbba0c53867945d095d9083b6aa
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-16 10:41:14 +0000

    Updated code

--- test/wacc/ast/NewPairNodeTest.java
+++ test/wacc/ast/NewPairNodeTest.java
@@ -6,21 +6,21 @@ import static org.junit.Assert.*;
 
 public class NewPairNodeTest {
 
-  private NewPairNode n;
+  private NewPairNode<ExprNode, ExprNode> n;
   private ExprNode e1 = new IntNode(5);
   private ExprNode e2 = new IntNode(10);
   private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000L);
 
   @Test
   public void pairElemInit() {
-    n = new NewPairNode(e1, e2);
+    n = new NewPairNode<>(e1, e2);
     assertTrue(n.getFst().equals(e1));
     assertTrue(n.getSnd().equals(e2));
   }
 
   @Test
   public void pairElemValidityT() {
-    n = new NewPairNode(e1, e2);
+    n = new NewPairNode<>(e1, e2);
     assertTrue(e1.isSemanticallyValid());
     assertTrue(e2.isSemanticallyValid());
     assertTrue(n.isSemanticallyValid());
@@ -28,10 +28,10 @@ public class NewPairNodeTest {
 
   @Test
   public void pairElemValidityF() {
-    n = new NewPairNode(e1, e3);
+    n = new NewPairNode<>(e1, e3);
     assertTrue(e1.isSemanticallyValid());
-    assertTrue(!e3.isSemanticallyValid());
-    assertTrue(!n.isSemanticallyValid());
+    assertFalse(e3.isSemanticallyValid());
+    assertFalse(n.isSemanticallyValid());
   }
 
 }
