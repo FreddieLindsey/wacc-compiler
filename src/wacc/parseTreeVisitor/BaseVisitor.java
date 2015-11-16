28c23327d56439a984c3b7225819018306a14366
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-16 16:49:27 +0000

    Improvement...

--- src/wacc/parseTreeVisitor/BaseVisitor.java
+++ src/wacc/parseTreeVisitor/BaseVisitor.java
@@ -1,4 +1,161 @@
 package wacc.parseTreeVisitor;
 
-public class BaseVisitor extends  {
+import antlr.BasicParser;
+import antlr.BasicParserVisitor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+public class BaseVisitor<ASTNode> implements BasicParserVisitor<ASTNode> {
+  @Override
+  public ASTNode visitArglist(@NotNull BasicParser.ArglistContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitCharliter(@NotNull BasicParser.CharliterContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitAssignlhs(@NotNull BasicParser.AssignlhsContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitParam(@NotNull BasicParser.ParamContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitBoolliter(@NotNull BasicParser.BoolliterContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitExpr(@NotNull BasicParser.ExprContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitArrayelem(@NotNull BasicParser.ArrayelemContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitType(@NotNull BasicParser.TypeContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitParamlist(@NotNull BasicParser.ParamlistContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitArraytype(@NotNull BasicParser.ArraytypeContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitIdent(@NotNull BasicParser.IdentContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitUnaryoper(@NotNull BasicParser.UnaryoperContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitAssignrhs(@NotNull BasicParser.AssignrhsContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitFunc(@NotNull BasicParser.FuncContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitStrliter(@NotNull BasicParser.StrliterContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitIntsign(@NotNull BasicParser.IntsignContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitBinaryOper(@NotNull BasicParser.BinaryOperContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitStat(@NotNull BasicParser.StatContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitArrayliter(@NotNull BasicParser.ArrayliterContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitPairtype(@NotNull BasicParser.PairtypeContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitBasetype(@NotNull BasicParser.BasetypeContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitProgram(@NotNull BasicParser.ProgramContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitPairliter(@NotNull BasicParser.PairliterContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitPairelem(@NotNull BasicParser.PairelemContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitPairelemtype(@NotNull BasicParser.PairelemtypeContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitIntliter(@NotNull BasicParser.IntliterContext ctx) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visit(@NotNull ParseTree parseTree) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitChildren(@NotNull RuleNode ruleNode) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitTerminal(@NotNull TerminalNode terminalNode) {
+    return null;
+  }
+
+  @Override
+  public ASTNode visitErrorNode(@NotNull ErrorNode errorNode) {
+    return null;
+  }
 }
