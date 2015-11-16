27948737eb4061c3f53a2753afc7591f0bba2444
James Prince <Prince@dyn1237-118.wlan.ic.ac.uk>
2015-11-16 16:24:00 +0000

    visitor progress

--- src/wacc/ast/ASTTree.java
+++ src/wacc/ast/ASTTree.java
@@ -1,7 +1,16 @@
 package wacc.ast;
 
 import org.antlr.v4.runtime.tree.ParseTree;
-import antlr.BasicParserBaseVisitor;
+import wacc.WACCParserBaseVisitor;
+
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 import java.util.Set;
 
@@ -10,15 +19,14 @@ public class ASTTree {
   private final ParseTree parseTree;
   private Set<String> semanticErrors;
   private ASTNode head;
-  private BasicParserBaseVisitor visitor;
+  private WACCParserBaseVisitor visitor;
 
   public ASTTree(ParseTree parseTree) {
     this.parseTree = parseTree;
   }
 
-  public ASTTree(ProgramContext p) {
-  	this.ProgramContext = p;
-  	visitor = new BasicParserBaseVisitor<ProgramNode>();
+  public ASTTree(BasicParser.ProgramContext p) {
+  	visitor = new WACCParserBaseVisitor();
   	head = visitProgram(p);
   }
 
