0233ede118ce36925cd9c65f979a9fb356870325
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-17 10:30:45 +0000

    Fixed CompStatNode

--- test/wacc/ast/CompStatNodeTest.java
+++ test/wacc/ast/CompStatNodeTest.java
@@ -9,8 +9,8 @@ import wacc.symbolTable.TypeEnum;
 public class CompStatNodeTest {
 
   private CompStatNode c;
-  private StatNode s1 = new BasicStatNode(StatTypeEnum.SKIP);
-  private StatNode s2 = new BasicStatNode(StatTypeEnum.RETURN); //invalid
+  private BasicStatNode s1 = new BasicStatNode(StatTypeEnum.SKIP);
+  private BasicStatNode s2 = new BasicStatNode(StatTypeEnum.RETURN); //invalid
   private ExprNode e1 = new ExprNode() {
     @Override
     public TypeEnum type() {
@@ -26,7 +26,6 @@ public class CompStatNodeTest {
 
   @Test
   public void compStatNodeInit() {
-    setParents();
     c = new CompStatNode(s1, s2);
     assertTrue(c.getFstStat().equals(s1));
     assertTrue(c.getSndStat().equals(s2));
@@ -34,20 +33,15 @@ public class CompStatNodeTest {
 
   @Test
   public void compStatNodeValidity() {
-    setParents();
-    c = new CompStatNode(s1, s2);
+    c = new CompStatNode(s1, s1);
     assertTrue(c.isSemanticallyValid());
   }
 
   @Test
   public void compStatNodeNotValid() {
-    setParents();
-    c = new CompStatNode(s1, s3);
+    s2.addExpr(e1);
+    c = new CompStatNode(s1, s2);
     assertTrue(!c.isSemanticallyValid());
-  }
-
-  private void setParents() {
-    ((BasicStatNode) s3).getExpr().setParent(s3);
   }
 
 }
