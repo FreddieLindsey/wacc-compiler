7497a92fb1a8376b305b44a540df004c414a9f25
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-17 14:27:50 +0000

    Had a beer and did it the lazy way

--- src/wacc/ast/type/ArrayElemNode.java
+++ src/wacc/ast/type/ArrayElemNode.java
@@ -1,37 +1,56 @@
 package wacc.ast.type;
 
+import wacc.ast.AssignNode;
 import wacc.ast.ExprNode;
 import wacc.ast.IdentNode;
+
 import java.util.ArrayList;
 
-public class ArrayElemNode extends ExprNode {
+public class ArrayElemNode extends AssignNode {
 
-  private final ArrayList<ExprNode> es;
-  private final IdentNode i;
-  private ArrayList<ExprNode> exprs;
+  private IdentNode ident;
+  private ArrayList<ExprNode> exprs = new ArrayList<>();
 
-  public ArrayElemNode(IdentNode i) {
+  public ArrayElemNode(IdentNode ident) {
     super();
-    this.i = i;
-    this.es = new ArrayList<ExprNode>();
-    i.setParent(this);
+    this.ident = ident;
+    ident.setParent(this);
+  }
+
+  public TypeNode type() {
+    return ident.type();
+  }
+
+  public IdentNode getId() {
+    return this.ident;
+  }
+
+  public ArrayList<ExprNode> getExprs() {
+    return this.exprs;
+  }
+
+  public void addExpr(ExprNode e) {
+    exprs.add(e);
+    e.setParent(this);
   }
 
   @Override
   public boolean isSemanticallyValid() {
+    for (ExprNode e : exprs) {
+      if (!e.isSemanticallyValid() || !e.type().equals(new TypeNode(TypeEnum.INT))) {
+        return false;
+      }
+    }
+    return ident.isSemanticallyValid();
+  }
+
+  @Override
+  public boolean validLeft() {
     return false;
   }
 
-  public void addExpr(ExprNode exprNode) {
-    es.add(exprNode);
-    exprNode.setParent(this);
-  }
-
-  public ArrayList<ExprNode> getExprs() {
-    return exprs;
-  }
-
-  public IdentNode getId() {
-    return i;
+  @Override
+  public boolean validRight() {
+    return true;
   }
 }
