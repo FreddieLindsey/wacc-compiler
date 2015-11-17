9954d938be1af37030d4793787e27a58ea5badcf
James Lane <james.lane14@imperial.ac.uk>
2015-11-17 17:13:35 +0000

    removed excess stuff

--- src/wacc/ast/pair/PairNode.java
+++ src/wacc/ast/pair/PairNode.java
@@ -1,18 +1,13 @@
 package wacc.ast.pair;
 
 import wacc.ast.AssignNode;
-import wacc.ast.type.*;
-import wacc.ast.ASTNode;
 import wacc.ast.ExprNode;
 import wacc.ast.type.TypeNode;
-import wacc.ast.type.TypeEnum;
 
 public class PairNode<F extends ExprNode, S extends ExprNode> extends AssignNode {
 
   private F fst;
   private S snd;	
-
-  private TypeNode type;	
   
   public PairNode(F fst, S snd) {
     super();
@@ -29,10 +24,6 @@ public class PairNode<F extends ExprNode, S extends ExprNode> extends AssignNode
 
   public S getSnd() {
     return this.snd;
-  }
-
-  public TypeNode getType() {
-    return this.type;
   }
 
   @Override
