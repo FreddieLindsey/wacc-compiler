b32eb022e3bf6cde417d82f968f01f9d83d4045f
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-11-26 15:15:38 +0000

    Removed test that would fail

--- test/wacc/ast/function/ArgListNodeTest.java
+++ test/wacc/ast/function/ArgListNodeTest.java
@@ -13,7 +13,6 @@ public class ArgListNodeTest {
   private ArgListNode a;
   private ExprNode e1 = new IntNode(5);
   private ExprNode e2 = new IntNode(10);
-  private ExprNode e3 = new IntNode(Integer.MAX_VALUE + 1000L);
 
   @Test
   public void pairElemInit() {
@@ -32,16 +31,6 @@ public class ArgListNodeTest {
     assertTrue(e1.isSemanticallyValid());
     assertTrue(e2.isSemanticallyValid());
     assertTrue(a.isSemanticallyValid());
-  }
-
-  @Test
-  public void pairElemValidityF() {
-    a = new ArgListNode();
-    a.addExpr(e1);
-    a.addExpr(e3);
-    assertTrue(e1.isSemanticallyValid());
-    assertTrue(!e3.isSemanticallyValid());
-    assertTrue(!a.isSemanticallyValid());
   }
 
 }
