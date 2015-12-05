2aab7fc9a695acd86709bd62033516837e18c369
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-12-05 15:53:26 +0000

    Cleaned up test

--- test/wacc/backend/AssemblyInstrTest.java
+++ test/wacc/backend/AssemblyInstrTest.java
@@ -9,42 +9,27 @@ public class AssemblyInstrTest {
 
   private AssemblyInstr instr;
 
-  private Arg arg1;
-  private Arg arg2;
-  private Arg arg3;
-
-  private ArrayList<Arg> args;
+  private ArrayList<Arg> args = new ArrayList<>();
 
 
   @Test
   public void assemInstrInit() {
-    args = new ArrayList<Arg>();
-
     args.add(new Label("link"));
 
     instr = new AssemblyInstr(AssemblyInstrEnum.B,
                        AssemblyInstrCond.NO_CODE, 
                        args);
 
-    System.err.println("assemInstrInit: " + instr.toString());
-    //assertTrue(true);
-
     assertTrue(instr.toString().equals("B link"));
   }
 
   @Test
   public void pushInstr() {
-    args = new ArrayList<Arg>();
-
-    arg1 = new Register(RegEnum.R0);
-    args.add(arg1);
+    args.add(new Register(RegEnum.R0));
 
     instr = new AssemblyInstr(AssemblyInstrEnum.PUSH,
                        AssemblyInstrCond.NO_CODE, 
                        args);
-
-    //System.err.println("pushInstr: " + instr.toString());
-    //assertTrue(true);
 
     assertTrue(instr.toString().equals("PUSH {r0}"));
   }
