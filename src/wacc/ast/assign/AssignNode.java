76555ed574b269a00800cb155a2ec6f6620c7d63
James Prince <Prince@Jamess-MBP-2.home>
2015-12-07 14:13:26 +0000

    done?

--- src/wacc/ast/assign/AssignNode.java
+++ src/wacc/ast/assign/AssignNode.java
@@ -1,12 +1,15 @@
 package wacc.ast.assign;
 
+import java.util.ArrayList;
+
 import wacc.ast.ExprNode;
+
+import wacc.backend.instruction.*;
 
 public abstract class AssignNode extends ExprNode {
 
   public abstract boolean validLeft();
 
   public abstract boolean validRight();
-
 
 }
