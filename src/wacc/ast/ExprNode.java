609f78e3ac34143acf18bd0198e096c6a90231f8
James Prince <Prince@Jamess-MBP-2.home>
2015-12-08 11:54:05 +0000

    rebased

--- src/wacc/ast/ExprNode.java
+++ src/wacc/ast/ExprNode.java
@@ -1,12 +1,7 @@
 package wacc.ast;
 
 import wacc.ast.type.TypeNode;
-<<<<<<< 23b3de1d77ceccd2e220958f0beada9c5a8d6713
 import wacc.backend.instruction.*;
-=======
-import wacc.backend.instruction.AssemblyInstr;
-import wacc.backend.instruction.InstructionBlock;
->>>>>>> Corrected for InstructionBlock
 
 
 import java.util.ArrayList;
