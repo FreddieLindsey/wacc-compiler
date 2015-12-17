ff3c61e4bfec7f857e4f74a49820ee870c48d47e
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-12-17 18:37:52 +0000

    Set sizes of components in windows to rough

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,26 +1,34 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class GUI {
 
-  private static final int WINDOW_WIDTH = 400;
+  private static final int WINDOW_WIDTH = 600;
   private static final int WINDOW_HEIGHT = 600;
 
   public static void main(String[] args) {
     JFrame window = new JFrame("WACC Compiler");
-    window.setSize(400, 600);
+    window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    window.setLocationRelativeTo(null);
 
     JPanel windowItems = new JPanel();
 
-    final JTextField input = new JTextField("Please type your WACC here", 40);
 
-    final JTextField output = new JTextField("Output will appear here", 40);
+    final Dimension fieldSize = new Dimension(WINDOW_WIDTH / 2 - 40, WINDOW_HEIGHT - 80);
+    final JTextField input = new JTextField("Please type your WACC here");
+    input.setPreferredSize(fieldSize);
+
+    final JTextField output = new JTextField("Output will appear here");
+    output.setPreferredSize(fieldSize);
 
     JButton compile = new JButton("Compile");
+    compile.setPreferredSize(new Dimension(WINDOW_WIDTH - 20, 20));
+
     compile.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
